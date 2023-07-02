2b1a40539eafb9231763cdb6049f141110a04827
AbhaySingh502 <abhaysingh65534@gmail.com>
2023-07-02 21:18:24 +0530

    login complee css

--- src/Js/Login.js
+++ src/Js/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import '../css/login.css'
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 function Login() {
   return (
     <div className='login'>
@@ -15,20 +16,25 @@ function Login() {
                     <div className='loginm_up'>
                             <div className='loginm_up_padd'>
                                 <h1>Sign in</h1>
-                                <div className='login_email'>
-
+                                <form className='login_email'>
+                                <h5> Email or mobile number</h5>
+                                <div className='input'><input type='text' placeholder='email'></input>
                                 </div>
+                                </form>
                                 <div className='login_button'>
-                                    <button>Continue</button>
+                                <button>Continue</button>
                                 </div>
                                 <div className='loginmm_median'>
-
+                                    <p>By continuing, you agree to Amazon's <a href='#'>Conditions of Use</a>and <a href='#'>Privacy Notice.</a>
+                                    </p>
                                 </div>
-                                <div className='loginmm_down'></div>
+                                <div className='loginmm_down'>
+                                    <ArrowRightIcon/><a href='#'>Need Help?</a>
+                                </div>
                             </div>
                     </div>
                     <div className='loginm_median'>
-                        <div className='line'>New to amazon</div>
+                        <div className='login_line'>New to amazon?</div>
                     </div>
                     <div className='loginm_down'>
                         <div className='loginm_but'>
@@ -38,7 +44,14 @@ function Login() {
                 </div>
             </div>
             <div className='login_down'>
-
+                <div className='upper'>
+                <div className='login1'><a href='#'>Condition of use</a></div>
+                <div className='login2'><a href='#'>Privacy Notice</a></div>
+                <div className='login3'><a href='#'>Help</a></div>
+                </div>
+                <div className='lower'>
+                © 1996-2023, Amazon.com, Inc. or its affiliates
+                </div>
             </div>
         </div>
       
